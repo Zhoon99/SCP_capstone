1e9e77e4bd2e6cfa11caa4afca83bd3d470a444f
rudals <rudals9901@gmail.com>
2022-03-20 03:07:11 +0900

    초기 설정

---
+++ scp/src/main/java/kr/mmgg/scp/ScpApplication.java
@@ -0,0 +1,13 @@
+package kr.mmgg.scp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpApplication.class, args);
+	}
+
+}
---
+++ scp/src/test/java/kr/mmgg/scp/ScpApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mmgg.scp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
