29f954ef9cb8b7cd4d2de3e245b61647d5bde835
LeNoad <71987963+LeNoad@users.noreply.github.com>
2022-03-23 18:10:37 +0900

    test homeservice 거의완성

--- scp/src/main/java/kr/mmgg/scp/service/homeService.java
+++ scp/src/main/java/kr/mmgg/scp/service/homeService.java
@@ -2,11 +2,15 @@ package kr.mmgg.scp.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import kr.mmgg.scp.dto.homeView_teamleader;
 import kr.mmgg.scp.dto.homeView_teammember;
+import kr.mmgg.scp.entity.Project;
+import kr.mmgg.scp.entity.ProjectInUser;
+import kr.mmgg.scp.repository.ProjectRepository;
 import kr.mmgg.scp.repository.ProjectinUserRepository;
 import kr.mmgg.scp.repository.TaskRepository;
 import lombok.AllArgsConstructor;
@@ -16,11 +20,13 @@ import lombok.AllArgsConstructor;
 public class homeService {
 	private ProjectinUserRepository projectinUserRepository;
 	private TaskRepository taskRepository;
+	private ProjectRepository projectRepository;
 	
 	public List<homeView_teamleader> homeView_leader(Long projectId) {
 		ArrayList<homeView_teamleader> list = new ArrayList<homeView_teamleader>();
 		homeView_teamleader homeview_teamleader = new homeView_teamleader();
-		homeview_teamleader.setProjectName(null);
+		Project project = projectRepository.findByProjectId(projectId);
+		homeview_teamleader.setProjectName(project.getProjectName());
 		homeview_teamleader.setTasklist(null);
 		list.add(homeview_teamleader);
 		return list;
@@ -28,10 +34,10 @@ public class homeService {
 	public List<homeView_teammember> homeView_member(Long projectId) {
 		ArrayList<homeView_teammember> list = new ArrayList<homeView_teammember>();
 		homeView_teammember homeview_teammember = new homeView_teammember();
-		homeview_teammember.setProjectName(null);
+		Project project = projectRepository.findByProjectId(projectId);
+		homeview_teammember.setProjectName(project.getProjectName());
 		homeview_teammember.setTasklist(null);
 		list.add(homeview_teammember);
 		return list;
 	}
-	
 }
