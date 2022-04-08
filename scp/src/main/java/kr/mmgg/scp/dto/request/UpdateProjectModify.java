0bac94ed55a406d1a46d4683e809521c06578eab
jamin12 <rudals9901@naver.com>
2022-04-08 21:54:37 +0900

    fileupload test완료

--- scp/src/main/java/kr/mmgg/scp/dto/request/UpdateProjectModify.java
+++ scp/src/main/java/kr/mmgg/scp/dto/request/UpdateProjectModify.java
@@ -1,11 +1,12 @@
 package kr.mmgg.scp.dto.request;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class UpdateProjectModify {
     private String projectName;
-    private Long userId;
     private Long projectId;
-    private String CommonCode;
+    private List<UpdateProjectModifyMember> users;
 }
