bcfd63909c382d9e01c225053081d8e533ab2200
jamin12 <rudals9901@naver.com>
2022-04-20 16:35:24 +0900

    interceptor 적용 완료

--- scp/src/main/java/kr/mmgg/scp/config/Webconfig.java
+++ scp/src/main/java/kr/mmgg/scp/config/Webconfig.java
@@ -2,12 +2,21 @@ package kr.mmgg.scp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.mmgg.scp.interceptor.AdminInterceptor;
 
 @Configuration
 public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // TODO Auto-generated method stub
+        registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/**");
     }
 }
