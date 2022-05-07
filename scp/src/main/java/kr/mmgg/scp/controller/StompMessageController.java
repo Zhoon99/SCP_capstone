15a1e7bacc48f299305995111cfaf9a1cca07365
LeNoad <71987963+LeNoad@users.noreply.github.com>
2022-05-08 00:20:57 +0900

    asd

--- scp/src/main/java/kr/mmgg/scp/controller/StompMessageController.java
+++ scp/src/main/java/kr/mmgg/scp/controller/StompMessageController.java
@@ -2,15 +2,12 @@ package kr.mmgg.scp.controller;
 
 import kr.mmgg.scp.dto.response.StompMessageDto;
 import kr.mmgg.scp.entity.User;
-import kr.mmgg.scp.repository.UserRepository;
 import kr.mmgg.scp.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,8 +17,7 @@ public class StompMessageController {
   private final UserService userService;
 
   @MessageMapping("/{room}/{userId}")
-  public void chat(@DestinationVariable("room") String room,
-                   @DestinationVariable("userId") Long userId , String content) throws Exception {
+  public void chat(@DestinationVariable("room") String room, @DestinationVariable("userId") Long userId , String content) throws Exception {
     User user = userService.findByUserId(userId);
     simpMessagingTemplate.convertAndSend("/topic/" + room , new StompMessageDto(user.getUserId(), user.getUserNickname(), content));
   }
