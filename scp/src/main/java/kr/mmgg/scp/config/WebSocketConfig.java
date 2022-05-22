a4fe72c3446fe6973847a3ffcdb301668f5de521
LeNoad <71987963+LeNoad@users.noreply.github.com>
2022-05-23 00:45:02 +0900

    Stomp Header Set

--- scp/src/main/java/kr/mmgg/scp/config/WebSocketConfig.java
+++ scp/src/main/java/kr/mmgg/scp/config/WebSocketConfig.java
@@ -1,15 +1,21 @@
 package kr.mmgg.scp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import kr.mmgg.scp.handler.StompHandler;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+	private final StompHandler stompHandler;
+	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic");
@@ -21,4 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		registry.addEndpoint("/chat").setAllowedOrigins("http://mmgg.kr").setAllowedOrigins("http://localhost")
 				.setAllowedOrigins("*");
 	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(stompHandler);
+	}
+	
 }
