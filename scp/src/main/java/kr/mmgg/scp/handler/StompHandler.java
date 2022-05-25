3d343560b7e01e67a6e85bff046563ed20ce55b2
rhddustjd <asdasd24242424@gmail.com>
2022-05-25 13:52:05 +0900

    StompHandler modify

--- scp/src/main/java/kr/mmgg/scp/handler/StompHandler.java
+++ scp/src/main/java/kr/mmgg/scp/handler/StompHandler.java
@@ -1,14 +1,14 @@
 package kr.mmgg.scp.handler;
 
 
+import java.util.Objects;
+
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
-
 import kr.mmgg.scp.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
 
@@ -19,10 +19,11 @@ public class StompHandler implements ChannelInterceptor{
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor acc = StompHeaderAccessor.wrap(message);
-		if(acc.getCommand() == StompCommand.CONNECT) {
-			if(!tokenProvider.validateToken(acc.getFirstNativeHeader("Authorization")))
-				throw new AccessDeniedException("");
+		System.out.println(message.getHeaders());
+		System.out.println(acc.getFirstNativeHeader("Authorization"));
+		if(StompCommand.CONNECT.equals(acc.getCommand())) {
+			tokenProvider.validateToken(Objects.requireNonNull(acc.getFirstNativeHeader("Authorization")).substring(7));
 		}
 		return message;
-	}
+	}	
 }
